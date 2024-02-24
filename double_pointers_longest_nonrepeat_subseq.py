7d8d8431d27c87c5b4f63f9954397499c55a478c
Xiaoya Wang <861366745@qq.com>
2024-02-24 20:13:24 +0800

    深入理解后增加了一些注释

--- double_pointers_longest_nonrepeat_subseq.py
+++ double_pointers_longest_nonrepeat_subseq.py
@@ -4,14 +4,14 @@ def main():
     len = int(input())
     seq = list(map(int, input().split()))
     longest_sub_len = 0
-    num_lst = [0] * 100010
+    num_lst = [0] * 100010 # 用于记录每个数字出现的次数
 
     j = 0 # j代表左端点
     for i in range(len): # i代表右端点
         # j和i都不走回头路
-        num_lst[seq[i]] += 1
-        while num_lst[seq[i]] > 1:
-            num_lst[seq[j]] -= 1
+        num_lst[seq[i]] += 1 # 每次新加的数字是seq[i]
+        while num_lst[seq[i]] > 1: 
+            num_lst[seq[j]] -= 1 # 每次减少的数字是seq[j]
             j += 1
         longest_sub_len = max(longest_sub_len, i-j+1)
 
