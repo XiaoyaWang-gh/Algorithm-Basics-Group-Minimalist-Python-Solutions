0b21c812b05ea0f1fd4564894cdefbecd80565be
Xiaoya Wang <861366745@qq.com>
2024-03-03 20:11:44 +0800

    数组模拟队列

--- chatpter2/queue.py
+++ chatpter2/queue.py
@@ -1,1 +1,40 @@
 # 用数组模拟队列：先进先出
+
+q = [0] * 100001
+hh = 0 #队头
+tt = -1 ##队尾
+
+def empty():
+    global hh,tt
+    if hh<=tt:
+        print("NO")
+    else:
+        print("YES")
+
+def push(x):
+    global tt
+    tt += 1
+    q[tt] = x
+
+def pop():
+    global hh
+    hh += 1
+
+def query():
+    print(q[hh])
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        op = input().split()
+        if op[0]=="push":
+            push(int(op[1]))
+        elif op[0]=="pop":
+            pop()
+        elif op[0]=="query":
+            query()
+        else:
+            empty()
+
+if __name__ == "__main__":
+    main()
